538754e6b024cfa4e34f329b4d1afdd04672963c
Alexander Vincent <AlexV9582@yahoo.com>
2017-10-04 12:42:17 -0400

    complete

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -4,30 +4,26 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends)
     });
-    app.post("/api.friends", function(req,res) {
+    app.post("/api/friends", function(req,res) {
         var bestMatch = {
             name  : "",
             pic   : "",
-            score : 1000
+            userScore : 1000
         }
-    console.log(req.body);
 
     var userData = req.body;
-    var userScores = req.scores;
-
-    console.log(userScores);
+    var userScores = userData.scores;
 
     var score = 0;
 
     for (i = 0; i < friends.length; i++) {
-        console.log(friends.length);
         score = 0;
-        for (j = 0; i < friends[i].scores[j]; j++) {
+        for (j = 0; j < friends[i].scores[j]; j++) {
             score = Math.abs(parseInt(userScores[j] - parseInt(friends[i].scores[j])));
-            if (score += bestMatch.score) {
+            if (score <= bestMatch.userScore) {
                 bestMatch.name = friends[i].name;
                 bestMatch.pic  = friends[i].photo;
-                bestMatch.score = score;
+                bestMatch.userScore = score;
             }
         }
     }
